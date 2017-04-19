ef0efa8fc64eb982d99b3b1dd8a6ec1eb5f21da0
Kara-wan <Kara-wan>
2017-04-19 16:45:17 +0800

    增加新的demo 和 注释

--- Example/HandyNoti/ViewController.swift
+++ Example/HandyNoti/ViewController.swift
@@ -11,21 +11,54 @@ import HandyNoti
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        NotificationCenter.default.addHandyObserver(self, selector: #selector(callBack), name: "handy", object: nil)
-        NotificationCenter.default.addHandyObserver(self, selector: #selector(callBack), name: "handy", object: nil)
-        NotificationCenter.default.addHandyObserver(self, selector: #selector(callBack), name: "handy", object: nil)
-        NotificationCenter.default.addHandyObserver(self, selector: #selector(callBack), name: "handy", object: nil)
-        NotificationCenter.default.addHandyObserver(self, selector: #selector(callBack), name: "handy", object: nil)
-        NotificationCenter.default.addHandyObserver(self, selector: #selector(callBack), name: "handy", object: nil)
-        NotificationCenter.default.removeHandyObserver(self)
+    @IBOutlet weak var oberserCountLabel: UILabel!
+    
+    @IBOutlet weak var handyAddBtn: UIButton!
+    
+    @IBOutlet weak var nativeAddBtn: UIButton!
+    
+    @IBOutlet weak var removeBtn: UIButton!
+
+    @IBOutlet weak var postBtn: UIButton!
+    
+//MARK:  -- Getter && Setter
+
+    fileprivate var observerCount:Int = 0{
+        didSet{
+            self.oberserCountLabel.text = "已监听" + String(observerCount) + "次"
+            if observerCount == 0 {
+                self.handyAddBtn.isEnabled = true
+                self.nativeAddBtn.isEnabled = true
+            }
+            self.postBtn.isEnabled = observerCount != 0
+        }
     }
     
+//MARK  -- EventResponse
     
-    override func viewDidAppear(_ animated: Bool) {
+    @IBAction func handyAddBtnClicked(_ sender: Any) {
+        nativeAddBtn.isEnabled = false
+        observerCount += 1
+        NotificationCenter.default.addHandyObserver(self, selector: #selector(callBack), name: "handy", object: nil)
+    }
+    
+    @IBAction func nativeAddBtnClicked(_ sender: Any) {
+        handyAddBtn.isEnabled = false
+        observerCount += 1
+        NotificationCenter.default.addObserver(self, selector: #selector(callBack), name: NSNotification.Name(rawValue: "handy"), object: nil)
+    }
+    
+    @IBAction func removeBtnClicked(_ sender: Any) {
+        observerCount = 0
+        removeObserver()
+    }
+    
+    @IBAction func postBtnClicked(_ sender: Any) {
+        callBackCount = 0
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: "handy"), object: nil)
     }
+
+//MARK  -- CallBack
     
     var callBackCount:Int = 0
     
@@ -33,10 +66,20 @@ class ViewController: UIViewController {
         callBackCount += 1
         print("第\(callBackCount)次回调")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+//MARK:  -- FilePrivateFunction
+    fileprivate final func removeObserver(){
+        NotificationCenter.default.removeHandyObserver(self)
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+//MARK:  -- LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    deinit {
+        removeObserver()
     }
 
 }
--- HandyNoti/Classes/HandyNoti.swift
+++ HandyNoti/Classes/HandyNoti.swift
@@ -1,21 +1,46 @@
 import Foundation
 
+
+/// 缓存池
 fileprivate var NotiPool:[NSObject:[String:Selector]] = [:]
 
 extension NotificationCenter {
     
+    /// 增加Handy观察者
+    ///
+    /// - Parameters:
+    ///   - observer: 观察者
+    ///   - aSelector: 回调方法
+    ///   - aName: 通知的名字
+    ///   - anObject: 回调数据
     open func addHandyObserver(_ observer: NSObject, selector aSelector: Selector, name aName: String, object anObject: Any?){
         
         guard !checkOutNotiPool(observer ,aName, newSel: aSelector) else {
             return
         }
+        
         registerNoti(observer, selector: aSelector, name: aName, object: anObject)
     }
     
+    
+    /// 增加Handy观察者
+    ///
+    /// - Parameter observer: 观察者
     open func removeHandyObserver(_ observer: NSObject){
+        guard NotiPool[observer] != nil else {
+            return
+        }
         NotiPool.removeValue(forKey: observer)
+        NotificationCenter.default.removeObserver(observer)
     }
     
+    
+    /// 移除观察者
+    ///
+    /// - Parameters:
+    ///   - observer: 观察者
+    ///   - aName: 通知名称
+    ///   - anObject: 回调数据
     open func removeHandyObserver(_ observer: NSObject, name aName: String, object anObject: Any?){
         guard NotiPool[observer] != nil else {
             return
@@ -24,9 +49,16 @@ extension NotificationCenter {
             return
         }
         NotiPool[observer]!.removeValue(forKey: aName)
-        self.removeObserver(observer, name: NSNotification.Name(rawValue: aName), object: anObject)
+        removeObserver(observer, name: NSNotification.Name(rawValue: aName), object: anObject)
     }
     
+    
+    /// 校验是否注册了通知
+    ///
+    /// - Parameters:
+    ///   - observer: 观察者
+    ///   - name: 通知名字
+    /// - Returns: 是否注册
     open func didRegisted(_ observer:NSObject , _ name:String)->Bool{
         guard NotiPool[observer] != nil else {
             return false
@@ -36,6 +68,13 @@ extension NotificationCenter {
     
     
     
+    /// 注册通知
+    ///
+    /// - Parameters:
+    ///   - observer: 观察者
+    ///   - aSelector: 回调方法
+    ///   - aName: 通知名称
+    ///   - anObject: 回调数据
     fileprivate final func registerNoti(_ observer: NSObject, selector aSelector: Selector, name aName: String, object anObject: Any?){
         var subPool:[String:Selector] = NotiPool[observer] ?? [:]
         subPool[aName] = aSelector
@@ -43,6 +82,16 @@ extension NotificationCenter {
         self.addObserver(observer, selector: aSelector, name: Notification.Name(rawValue: aName), object: anObject)
     }
     
+    
+    /// 1.检查缓存池中是否存在该观察者，
+    /// 2.该观察者是否已经注册了通知(正常情况下是1的充要条件)
+    /// 2.已经监听的通知回调方法和新方法一致？
+    ///
+    /// - Parameters:
+    ///   - observer: 观察者
+    ///   - notiName: 通知名称
+    ///   - newSel: 新的回调方法
+    /// - Returns: 检查结果
     fileprivate final func checkOutNotiPool(_ observer: NSObject , _ notiName:String,newSel:Selector)->Bool{
         
         if let subPool:[String:Selector] = NotiPool[observer] {
